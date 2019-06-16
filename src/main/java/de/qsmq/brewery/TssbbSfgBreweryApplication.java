dc85db84aa66d968fcd2502eff0968cf47a18fba
Ralph L <lettau.ralph@gmail.com>
2019-06-16 21:42:45 +0200

    Inital commit

---
+++ src/main/java/de/qsmq/brewery/TssbbSfgBreweryApplication.java
@@ -0,0 +1,13 @@
+package de.qsmq.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TssbbSfgBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TssbbSfgBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/qsmq/brewery/TssbbSfgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.qsmq.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TssbbSfgBreweryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
